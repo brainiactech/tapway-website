6fa76edb744895ddaffda8bf3f529abd55c7c1cd
Olutunmbi Banto <olutunmbi@nairabox.com>
2019-11-16 14:25:46 +0100

    rider and merchant reg

--- js/contact.js
+++ js/contact.js
@@ -5,34 +5,70 @@
    File Description: Contact Form
 */
 
-$('#contact-form').submit(function() {
-    var action = $(this).attr('action');
-    $("#message").slideUp(750, function() {
-        $('#message').hide();
+$('#contact-form').submit(function () {
+	var action = $(this).attr('action');
+	$("#message").slideUp(750, function () {
+		$('#message').hide();
 
-        $('#submit')
-            .before('')
-            .attr('disabled', 'disabled');
+		$('#submit')
+			.before('')
+			.attr('disabled', 'disabled');
 
-        $.post(action, {
-                name: $('#name').val(),
-                email: $('#email').val(),
-                comments: $('#comments').val(),
-            },
-            function(data) {
-                document.getElementById('message').innerHTML = data;
-                $('#message').slideDown('slow');
-                $('#cform img.contact-loader').fadeOut('slow', function() {
-                    $(this).remove()
-                });
-                $('#submit').removeAttr('disabled');
-                if (data.match('success') != null) $('#cform').slideUp('slow');
-            }
-        );
+		$.post(action, {
+			name: $('#name').val(),
+			email: $('#email').val(),
+			comments: $('#comments').val(),
+		},
+			function (data) {
+				document.getElementById('message').innerHTML = data;
+				$('#message').slideDown('slow');
+				$('#cform img.contact-loader').fadeOut('slow', function () {
+					$(this).remove()
+				});
+				$('#submit').removeAttr('disabled');
+				if (data.match('success') != null) $('#cform').slideUp('slow');
+			}
+		);
 
-    });
+	});
 
-    return false;
+	return false;
 
 });
+
+
+$('#rider-reg').submit(function () {
+	var action = $(this).attr('action');
+	$("#message").slideUp(750, function () {
+		$('#message').hide();
+
+		$('#submit')
+			.before('')
+			.attr('disabled', 'disabled');
+
+		$.post(action, {
+			fullname: $('#fullname').val(),
+			email: $('#email').val(),
+			address: $('#address').val(),
+			phone: $('#phone').val(),
+			city: $('#city').val(),
+			state: $('#state').val(),
+		},
+			function (data) {
+				document.getElementById('message').innerHTML = data;
+				$('#message').slideDown('slow');
+				$('#cform img.contact-loader').fadeOut('slow', function () {
+					$(this).remove()
+				});
+				$('#submit').removeAttr('disabled');
+				if (data.match('success') != null) $('#cform').slideUp('slow');
+			}
+		);
+
+	});
+
+	return false;
+
+});
+
 
